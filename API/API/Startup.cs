113e010081fb3fb4725dc8d74df76dd4a00eb595
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-21 00:02:29 +0200

    Added Cors to API

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -23,6 +23,13 @@ namespace API
             services.AddControllers();
             services.AddDbContext<DataContext>(opt =>
                 opt.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +43,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
