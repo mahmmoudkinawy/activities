a4175fa20b0157800daefc8f269bb22e7d961351
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-21 00:05:38 +0200

    Modified Core from services to Middleware

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -23,13 +23,6 @@ namespace API
             services.AddControllers();
             services.AddDbContext<DataContext>(opt =>
                 opt.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +37,10 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors("CorsPolicy");
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("http://localhost:3000"));
 
             app.UseAuthorization();
 
