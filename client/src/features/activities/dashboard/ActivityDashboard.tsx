36da0f8b287af7c95a755ea96c5193cfc0eb1c16
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-22 17:08:46 +0200

    Create and Edit is now from the API

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, 
@@ -25,7 +26,8 @@ export default function ActivityDashboard({activities,
         openForm,
         closeForm,
         createOrEdit,
-        deleteActivity}: Props) {
+        deleteActivity,
+        submitting }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -39,7 +41,7 @@ export default function ActivityDashboard({activities,
                     openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm submitting={submitting} closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
             </Grid.Column>
         </Grid>
     )
