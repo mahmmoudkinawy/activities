09b349aa07b67e41a62d47b9324cfa1c6a6ef056
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-23 12:40:44 +0200

    Refactoring

--- client/src/App/layout/App.tsx
+++ client/src/App/layout/App.tsx
@@ -6,6 +6,7 @@ import { Route } from 'react-router';
 import HomePage from '../../features/home/HomePage';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
  
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <NavBar />
       <Container style={{marginTop: '7em'}}>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/activities' component={ActivityDashboard}/>
+        <Route exact path='/activities' component={ActivityDashboard}/>
+        <Route path='/activities/:id' component={ActivityDetails}/>
         <Route path='/create-activity' component={ActivityForm}/>
       </Container>
     </Fragment>
