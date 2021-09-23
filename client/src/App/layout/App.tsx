650909091d30e455e967eadadb4580e597f633a1
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-23 15:39:41 +0200

    Fixed create-acivity path

--- client/src/App/layout/App.tsx
+++ client/src/App/layout/App.tsx
@@ -2,13 +2,15 @@ import { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router';
+import { Route, useLocation } from 'react-router';
 import HomePage from '../../features/home/HomePage';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
  
 function App() {
+  const location = useLocation();
+
   return (
     <Fragment>
       <NavBar />
@@ -16,7 +18,7 @@ function App() {
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/activities' component={ActivityDashboard}/>
         <Route path='/activities/:id' component={ActivityDetails}/>
-        <Route path={['/create-activity', '/manage/:id']} component={ActivityForm}/>
+        <Route key={location.key} path={['/create-activity', '/manage/:id']} component={ActivityForm}/>
       </Container>
     </Fragment>
   );
