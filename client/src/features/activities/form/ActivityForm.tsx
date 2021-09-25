c08d6a514e5d1aeda5d68242688a57fe5d8d6993
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-25 16:10:01 +0200

    Added text area reusable component

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import MyTextInput from "../../../App/common/form/MyTextInput";
+import MyTextArea from "../../../App/common/form/MyTextArea";
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -75,7 +76,7 @@ export default observer(function ActivityForm() {
         {({ handleSubmit }) => (
           <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
             <MyTextInput name="title" placeholder="Title" />
-            <MyTextInput placeholder="Description" name="description" />
+            <MyTextArea rows={3} placeholder="Description" name="description" />
             <MyTextInput placeholder="Category" name="category" />
             <MyTextInput placeholder="Date" name="date" />
             <MyTextInput placeholder="City" name="city" />
