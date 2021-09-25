0e4d892d659596831b2b13d946c32277efb113e5
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-25 12:46:26 +0200

    Added the rest of error handling

--- client/src/App/api/agent.ts
+++ client/src/App/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity } from "../models/activity";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -17,9 +18,15 @@ axios.interceptors.response.use(
     return response;
   },
   (error: AxiosError) => {
-    const { data, status } = error.response!;
+    const { data, status, config } = error.response!;
     switch (status) {
       case 400:
+        if (typeof data === "string") {
+          toast.error(data);
+        }
+        if (config.method === "get" && data.errors.hasOwnProperty("id")) {
+          history.push("/not-found");
+        }
         if (data.errors) {
           const modelStateErrors = [];
           for (const key in data.errors) {
@@ -28,8 +35,6 @@ axios.interceptors.response.use(
             }
           }
           throw modelStateErrors.flat();
-        } else {
-          toast.error(data);
         }
         break;
       case 401:
@@ -39,7 +44,8 @@ axios.interceptors.response.use(
         history.push("/not-found");
         break;
       case 500:
-        toast.error("server error");
+        store.commonStore.setServerError(data);
+        history.push("/server-error");
         break;
     }
     return Promise.reject(error);
