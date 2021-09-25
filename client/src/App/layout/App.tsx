71afd3b5843e9ee0e2b5f42e81f5b718b91bdca7
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-25 11:32:38 +0200

    Added errors to the UI

--- client/src/App/layout/App.tsx
+++ client/src/App/layout/App.tsx
@@ -1,31 +1,38 @@
-import { Fragment } from 'react';
-import { Container } from 'semantic-ui-react';
-import NavBar from './NavBar';
-import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router';
-import HomePage from '../../features/home/HomePage';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import ActivityForm from '../../features/activities/form/ActivityForm';
-import ActivityDetails from '../../features/activities/details/ActivityDetails';
- 
+import { Fragment } from "react";
+import { Container } from "semantic-ui-react";
+import NavBar from "./NavBar";
+import { observer } from "mobx-react-lite";
+import { Route, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestError";
+
 function App() {
   const location = useLocation();
 
   return (
-    <Fragment>      
-
-      <Route exact path='/' component={HomePage}/>
-      <Route path={'/(.+)'} render={() => (
-        <Fragment>
-          <NavBar />        
-          <Container style={{marginTop: '7em'}}>
-            <Route exact path='/activities' component={ActivityDashboard}/>
-            <Route path='/activities/:id' component={ActivityDetails}/>
-            <Route key={location.key} path={['/create-activity', '/manage/:id']} component={ActivityForm}/>
-          </Container>
-        </Fragment>
-      )}/>
-
+    <Fragment>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
+              <Route exact path="/activities" component={ActivityDashboard} />
+              <Route path="/activities/:id" component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={["/create-activity", "/manage/:id"]}
+                component={ActivityForm}
+              />
+              <Route path="/errors" component={TestErrors} />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 }
--- client/src/App/layout/NavBar.tsx
+++ client/src/App/layout/NavBar.tsx
@@ -4,18 +4,28 @@ import { Button, Container, Menu } from "semantic-ui-react";
 import { useStore } from "../stores/store";
 
 export default function NavBar() {
-    return (
-        <Menu inverted fixed='top'>
-            <Container>
-                <Menu.Item as={NavLink} to='/' exact header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
-                    Reactivities
-                </Menu.Item>
-                <Menu.Item as={NavLink} to='/activities' name="Activities" />
-                <Menu.Item>
-                    <Button as={NavLink} to='/create-activity' positive content="Create Activity"/>
-                </Menu.Item>
-            </Container>
-        </Menu>
-    )
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item as={NavLink} to="/" exact header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/activities" name="Activities" />
+        <Menu.Item as={NavLink} to="/errors" name="Errors" />
+        <Menu.Item>
+          <Button
+            as={NavLink}
+            to="/create-activity"
+            positive
+            content="Create Activity"
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
 }
