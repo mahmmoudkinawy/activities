7e989fb4a30fcb727305d0afe7cbf84e8af1a0ed
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-27 14:54:54 +0200

    Added related entities and auto mapper

--- API/Application/Activities/ActivityDto.cs
+++ API/Application/Activities/ActivityDto.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Application.Profiles;
+using System;
 using System.Collections.Generic;
 
-namespace Domain
+namespace Application.Activities
 {
-    public class Activity
+    public class ActivityDto
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
@@ -13,6 +14,7 @@ namespace Domain
         public string City { get; set; }
         public string Venue { get; set; }
 
-        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
+        public string HostUsername { get; set; }
+        public ICollection<Profile> Profiles { get; set; }
     }
 }
--- API/Application/Activities/List.cs
+++ API/Application/Activities/List.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Core;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,20 +13,29 @@ namespace Application.Activities
     public class List
     {
 
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
-            public async Task<Result<List<Activity>>> Handle(Query request,
+            public async Task<Result<List<ActivityDto>>> Handle(Query request,
                 CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+                var activites = await _context.Activities
+                    .Include(x => x.Attendees)
+                    .ThenInclude(u => u.AppUser)
+                    .ToListAsync(cancellationToken);
+
+                var activitesToReturn = _mapper.Map<List<ActivityDto>>(activites);
+
+                return Result<List<ActivityDto>>.Success(activitesToReturn);
             }
         }
     }
