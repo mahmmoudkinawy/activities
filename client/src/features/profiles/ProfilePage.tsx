8c61b3564c435b08f8dd9a559d91a8af4b65e54e
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-01 00:21:46 +0200

    Attatch content from API to the UI

--- client/src/features/profiles/ProfilePage.tsx
+++ client/src/features/profiles/ProfilePage.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import ProfileHeader from "./ProfileHeader";
 import ProfileContent from "./ProfileContent";
+import { observer } from "mobx-react-lite";
+import { useParams } from "react-router";
+import { useStore } from "../../App/stores/store";
+import LoadingComponent from "../../App/layout/LoadingComponent";
 
-export default function ProfilePage() {
+export default observer(function ProfilePage() {
+  const { username } = useParams<{ username: string }>();
+  const { profileStore } = useStore();
+  const { loadingProfile, loadProfile, profile } = profileStore;
+
+  useEffect(() => {
+    loadProfile(username);
+  }, [loadProfile, username]);
+
+  if (loadingProfile) return <LoadingComponent content="Loading profile..." />;
+
   return (
     <Grid>
       <Grid.Column width={16}>
-        <ProfileHeader />
+        {profile && <ProfileHeader profile={profile} />}
         <ProfileContent />
       </Grid.Column>
     </Grid>
   );
-}
+});
