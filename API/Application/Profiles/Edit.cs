d7736473d7c2d26050a7a9e400db0ebcfc4e2052
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-03 11:18:49 +0200

    Added update profile in the API

---
+++ API/Application/Profiles/Edit.cs
@@ -0,0 +1,59 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class Edit
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string DisplayName { get; set; }
+            public string Bio { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.DisplayName).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly IUserAccessor _userAccessor;
+            private readonly DataContext _context;
+
+            public Handler(IUserAccessor userAccessor, DataContext context)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request,
+                CancellationToken cancellationToken)
+            {
+                var user = await _context.Users
+                    .FirstOrDefaultAsync(u => u.UserName == _userAccessor.GetUsername());
+
+                user.Bio = request.Bio ?? user.Bio;
+                user.DisplayName = request.DisplayName ?? user.DisplayName;
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success)
+                    return Result<Unit>.Success(Unit.Value);
+
+                return Result<Unit>.Failure("Problem updating profile");
+            }
+        }
+
+    }
+}
