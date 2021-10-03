563787c1924c35dc561abb6a56332930a9aa799b
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-03 11:40:10 +0200

    Added updating profile in UI

--- client/src/features/profiles/ProfileCard.tsx
+++ client/src/features/profiles/ProfileCard.tsx
@@ -1,20 +1,25 @@
-import React from "react";
 import { observer } from "mobx-react-lite";
-import { Profile } from "../../App/models/profile";
-import { Card, Icon, Image } from "semantic-ui-react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { Card, Icon, Image } from "semantic-ui-react";
+import { Profile } from "../../App/models/profile";
 
 interface Props {
   profile: Profile;
 }
 
 export default observer(function ProfileCard({ profile }: Props) {
+  function truncate(str: string | undefined) {
+    if (str) {
+      return str.length > 40 ? str.substring(0, 37) + "..." : str;
+    }
+  }
   return (
     <Card as={Link} to={`/profiles/${profile.username}`}>
-      <Image src={profile.image || "./assets/user.png"} />
+      <Image src={profile.image || "/assets/user.png"} />
       <Card.Content>
         <Card.Header>{profile.displayName}</Card.Header>
-        <Card.Description>Bio goes here</Card.Description>
+        <Card.Description>{truncate(profile.bio)}</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <Icon name="user" />
