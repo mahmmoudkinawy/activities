daee74cc31c8994937e2619f0b31b91176386117
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-10-05 15:59:04 +0200

    Added filter to API and UI

--- API/Application/Activities/List.cs
+++ API/Application/Activities/List.cs
@@ -3,7 +3,6 @@ using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System.Linq;
 using System.Threading;
@@ -16,7 +15,7 @@ namespace Application.Activities
 
         public class Query : IRequest<Result<PagedList<ActivityDto>>>
         {
-            public PagingParams Params { get; set; }
+            public ActivityParams Params { get; set; }
         }
 
         public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
@@ -35,12 +34,23 @@ namespace Application.Activities
                 CancellationToken cancellationToken)
             {
                 var query = _context.Activities
+                    .Where(d => d.Date >= request.Params.StartDate)
                     .OrderBy(d => d.Date)
                     .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new
                     {
                         currentUsername = _userAccessor.GetUsername()
                     })
                     .AsQueryable();
+
+                if (request.Params.IsGoing && !request.Params.IsHost)
+                {
+                    query = query.Where(x => x.Attendees.Any(a => a.Username == _userAccessor.GetUsername()));
+                }
+
+                if(request.Params.IsHost && !request.Params.IsGoing)
+                {
+                    query = query.Where(x => x.HostUsername == _userAccessor.GetUsername());
+                }
 
                 return Result<PagedList<ActivityDto>>.Success(
                     await PagedList<ActivityDto>.CreateAsync(query,
